2c746150b5e95994a61418f3cc6a26bcf1ae0f76
Bafana <bafana.mahamba@sovtech.com>
2022-10-20 01:14:27 +0200

    remove trailing white space

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * print_alphabet -> prints alphabets
+ * prin_alphabet - function to print abc
+ *
+ * Return: 0
  */
-
 void print_alphabet(void)
 {
-	int j;
-
-	for (j = 'a'; j <= 'z'; j++)
+	char c;
+	
+	for (c = 'a'; c <= 'z'; c++)
 	{
-		_putchar(j);
+		_putchar(c);
 	}
-	_putchar('\n');
+_putchar('\n');
 }
